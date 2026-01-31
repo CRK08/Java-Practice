edf92e91a6202e023559e80d9b35c9f4e1ddcb08
Kaviyarasan C <crkaviyarasan1029@gmail.com>
2026-01-31 11:38:51 +0530

    Maximum of Two Numbers using Ternary Operator

---
+++ Sequence/Max_Two_nos.java
@@ -0,0 +1,8 @@
+class Max_Two_nos{
+    public static void main(String[] args) {
+        //Maximum Number using Ternary Operator
+        int num = 10;
+		String result = (num % 2 == 0)?"even":"odd";
+		System.out.println(result);   
+    }
+}
