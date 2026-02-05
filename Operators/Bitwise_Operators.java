8e9c65caccaece55d4168b7d933f8f91e253635e
Kaviyarasan C <crkaviyarasan1029@gmail.com>
2026-02-05 21:26:38 +0530

    Operators: Demonstrates bitwise operators (&, ^, |)

---
+++ Operators/Bitwise_Operators.java
@@ -0,0 +1,9 @@
+public class Bitwise_Operators {
+    public static void main(String[] args) {
+        // Demonstrates bitwise operators (&, ^, |)
+        int i = 423, j = 526;
+        System.out.println(i & j);  // Bitwise AND
+        System.out.println(i ^ j);  // Bitwise XOR
+        System.out.println(i | j);  // Bitwise OR
+    }
+}
