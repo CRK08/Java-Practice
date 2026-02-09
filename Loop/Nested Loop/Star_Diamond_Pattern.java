484dd4ee7a71756f4077d65f8f7c6d16d5d5f842
Kaviyarasan C <crkaviyarasan1029@gmail.com>
2026-02-09 22:37:49 +0530

    Nested Loop: Printing Diamond using stars

---
+++ Loop/Nested Loop/Star_Diamond_Pattern.java
@@ -0,0 +1,24 @@
+class Star_Diamond_Pattern {
+    public static void main(String[] args) {
+        // Print upper half of diamond
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= (2 * i) - 1; k++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        // Print lower half of diamond
+        for (int i = 4; i >= 1; i--) {
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= (2 * i) - 1; k++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
