f8b6923b2cec2c4f22371587fcd41f1b3d9521a8
Kaviyarasan C <crkaviyarasan1029@gmail.com>
2026-02-18 22:49:11 +0530

    Matrix: Sum of all in Two Dimensional Array Using Jagged Array

---
+++ Two Dimensional Arrays/TwoD_Sum_Ele.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class TwoD_Sum_Ele{
+    public static void main(String[] args) {
+        //Sum of all in Two Dimensional Array Using Jagged Array
+        Scanner ab = new Scanner(System.in);
+        int r1,c1,sum=0;
+        System.out.println("Enter Row Size:");
+        r1 = ab.nextInt();
+        int[][] a1=new int[r1][];
+        for(int i=0;i<a1.length;i++){
+            System.out.println("Enter Coulmn "+ (i+1)+" size");
+            c1=ab.nextInt();
+            System.out.println("Enter Elements:");
+            a1[i]=new int[c1];
+            for(int j=0;j<c1;j++){
+                a1[i][j]=ab.nextInt();
+            }
+        }
+        for(int i=0;i<a1.length;i++){
+            for(int j=0;j<a1[i].length;j++){
+                sum+=a1[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
